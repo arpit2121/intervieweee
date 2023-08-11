5cb634876b291494589d7425b818bc6439300145
arpit2121 <arpitsingh9892@gmail.com>
2023-08-11 18:11:50 +0530

    dot icon

--- src/components/icons/recorder/RecordingDotIcon.jsx
+++ src/components/icons/recorder/RecordingDotIcon.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import useResponsiveStyles from "../../../utils/MediaQuery";
 
-const RecordingDotIcon = ({onclick}) => {
-  const responsive = useResponsiveStyles();
+const RecordingDotIcon = () => {
   return (
     <svg
-      width={responsive.isMobile? "12" :'18'}
-      height={responsive.isMobile? "12" :'18'}
+      width={responsive.isMobile ? "12" : "18"}
+      height={responsive.isMobile ? "12" : "18"}
       viewBox="0 0 18 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
